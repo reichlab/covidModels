4cb78f657cc57314cf9085ce273d2477866285a2
matthewcornell <matt@matthewcornell.org>
2022-02-10 11:20:09 -0500

    cleaned up run-baseline.sh

--- aws-vm-scripts/run-baseline.sh
+++ aws-vm-scripts/run-baseline.sh
@@ -67,28 +67,33 @@ if [ $? -eq 0 ]; then
   else
     slack_message "PDF_DIR success: PDF_DIR=${PDF_DIR}. date=$(date), uname=$(uname -a)"
 
-    # create and push branch with new CSV file
+    # create and push branch with new CSV file. we could first sync w/upstream and then push to the fork, but this is
+    # unnecessary for this script because `make all` gets the data it needs from the net and not the ${HUB_DIR}. note
+    # that we may later decide to go ahead and sync in case other scripts that use this volume (at /data) need
+    # up-to-date data, which is why we've kept the commands but commented them out
     HUB_DIR="/data/covid19-forecast-hub"
     slack_message "updating HUB_DIR=${HUB_DIR}. date=$(date), uname=$(uname -a)"
     cd "${HUB_DIR}"
-    git fetch upstream                            # pull down the latest source from original repo - https://github.com/reichlab/covid19-forecast-hub
-    git pull upstream master                      # update fork from original repo to keep up with their changes
+    # git fetch upstream                         # pull down the latest source from original repo - https://github.com/reichlab/covid19-forecast-hub
+    # git checkout master                        # ensure I'm on local master
+    # git merge upstream/master                  # update fork from original repo to keep up with their changes
+    # git push origin master                     # sync with fork
 
     MONDAY_DATE=$(basename ${PDF_DIR})            # e.g., 2022-01-31
     NEW_BRANCH_NAME="baseline-${MONDAY_DATE//-/}" # remove '-'. per https://tldp.org/LDP/abs/html/string-manipulation.html
     CSV_DIR="${COVID_MODELS_DIR}/weekly-submission/forecasts/COVIDhub-baseline"
 
-    slack_message "creating branch and pushing. MONDAY_DATE=${MONDAY_DATE}, NEW_BRANCH_NAME=${NEW_BRANCH_NAME}. date=$(date), uname=$(uname -a)"
+    slack_message "creating branch and pushing. MONDAY_DATE=${MONDAY_DATE}, NEW_BRANCH_NAME=${NEW_BRANCH_NAME}, CSV_DIR=${CSV_DIR}. date=$(date), uname=$(uname -a)"
     git checkout -b ${NEW_BRANCH_NAME}
     cp ${CSV_DIR}/*.csv "${HUB_DIR}/data-processed/COVIDhub-baseline"
-    git add -A
+    git add data-processed/COVIDhub-baseline/\*
     git commit -m "baseline build, ${MONDAY_DATE}"
-    git push --set-upstream origin ${NEW_BRANCH_NAME}
+    git push origin ${NEW_BRANCH_NAME} # unsure whether `--set-upstream` needed. seems OK without it
     PUSH_RESULT=$?
 
     if [ $PUSH_RESULT -eq 0 ]; then
       ORIGIN_URL=$(git config --get remote.origin.url) # e.g., https://github.com/reichlabmachine/covid19-forecast-hub.git
-      ORIGIN_URL=${ORIGIN_URL::-4}                     # assumes original clone included ".git". todo one expression
+      ORIGIN_URL=${ORIGIN_URL::-4}                     # assumes original clone included ".git"
       slack_message "push OK. CVS branch=${ORIGIN_URL}/tree/${NEW_BRANCH_NAME}. date=$(date), uname=$(uname -a)"
     else
       slack_message "push failed. date=$(date), uname=$(uname -a)"
@@ -99,8 +104,6 @@ if [ $? -eq 0 ]; then
     git branch -D ${NEW_BRANCH_NAME}                 # remove baseline branch from local
 
     # done with branch. upload PDFs, and optionally zipped CSV file (if push failed)
-    ORIGIN_URL=$(git config --get remote.origin.url) # e.g., https://github.com/reichlabmachine/covid19-forecast-hub.git
-    ORIGIN_URL=${ORIGIN_URL::-4}                     # todo one expression
     slack_message "uploading log, PDFs, [CSVs]. date=$(date), uname=$(uname -a)"
     slack_upload ${OUT_FILE}
 
@@ -110,8 +113,6 @@ if [ $? -eq 0 ]; then
 
     if [ $PUSH_RESULT -ne 0 ]; then
       for CSV_FILE in ${CSV_DIR}/*.csv; do
-        # MONDAY_DATE=$(basename ${PDF_DIR}) # e.g., 2022-01-31
-        # ZIP_CSV_FILE=/tmp/${CSV_FILE}.zip
         ZIP_CSV_FILE=/tmp/$(basename ${CSV_FILE}).zip
         zip ${ZIP_CSV_FILE} ${CSV_FILE}
         slack_upload ${ZIP_CSV_FILE}
