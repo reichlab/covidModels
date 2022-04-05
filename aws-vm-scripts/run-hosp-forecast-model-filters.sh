a72b2cb786e2b3a9a157d7df917c644a5325e9a5
matthewcornell <matt@matthewcornell.org>
2022-04-05 15:40:06 -0400

    save the html output file to the HUB_DIR root

--- aws-vm-scripts/run-hosp-forecast-model-filters.sh
+++ aws-vm-scripts/run-hosp-forecast-model-filters.sh
@@ -14,26 +14,41 @@ set +o allexport
 # load slack functions - per https://stackoverflow.com/questions/10822790/can-i-call-a-function-of-a-shell-script-from-another-shell-script/42101141#42101141
 source $(dirname "$0")/slack.sh
 
+#
 # start
+#
+
 slack_message "starting"
 
-# sync covid19-forecast-hub fork with upstream
-slack_message "updating covid19-forecast-hub fork"
-HUB_DIR="/data/covid19-forecast-hub" # a fork
+HUB_DIR="/data/covid19-forecast-hub"         # a fork
+HUB_WEB_DIR="/data/covid19-forecast-hub-web" # not a fork
+slack_message "updating HUB_DIR=${HUB_DIR} and HUB_WEB_DIR=${HUB_WEB_DIR}"
+
+# sync covid19-forecast-hub fork with upstream. note that we do not pull changes from the fork because we frankly don't
+# need them; all we're concerned with is adding new files to a new branch and pushing them.
 cd "${HUB_DIR}"
 git fetch upstream # pull down the latest source from original repo
 git checkout master
 git merge upstream/master # update fork from original repo to keep up with their changes
 
+cd ${HUB_WEB_DIR}
+git pull
+
 # knit the file
 slack_message "knitting the file"
+cd "${HUB_DIR}"
 Rscript -e "library(knitr); rmarkdown::render('code/reports/hospital-model-forecaster-filtering.Rmd')"
 
-# publish output file. todo xx add to GitHub Pages repo, commit, push, print message (link to page), etc.
-# for now simply upload the file
+# save the html output file to the HUB_DIR root. NB: we do *not* update the web site by running the deploy GitHub Action
+# as we do in run-weekly-reports.sh (see `gh workflow run blank.yml`) because it's not important enough to warrant the
+# effort. we know a deploy happens in the evening anyway
 OUT_FILE=${HUB_DIR}/code/reports/hospital-model-forecaster-filtering.html
-slack_message "knit done"
-slack_upload ${OUT_FILE}
+slack_message "knit done, copying OUT_FILE=${OUT_FILE} to HUB_DIR=${HUB_DIR} root"
+cd ${HUB_WEB_DIR}
+cp -f ${OUT_FILE} ${HUB_WEB_DIR}
+git add ${OUT_FILE}
+git commit -m "Latest hospital model forecaster filtering file"
+git push
 
 # done
 slack_message "done. shutting down"
